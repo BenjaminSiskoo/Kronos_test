d9532dca2bd2b5b99ad92f52c33250e7a3e1888c
Benjamin Siskoo <siskoo@tradu-france.com>
2026-04-02 00:29:14 +0200

    vdp1: fix user clipping local coord reset condition (spec §3.2)

--- yabause/src/core/video/opengl/compute_shader/src/vidcs.c
+++ yabause/src/core/video/opengl/compute_shader/src/vidcs.c
@@ -881,13 +881,13 @@ void VIDCSVdp1LineDraw(vdp1cmd_struct *cmd, u8 * ram, Vdp1 * regs)
 
 void VIDCSVdp1UserClipping(vdp1cmd_struct *cmd, u8 * ram, Vdp1 * regs)
 {
-	// APRÈS – la spec dit OR : si l'un OU l'autre dépasse, reset local coords
-	if (  (cmd->CMDXC > regs->systemclipX2)
-	  || (cmd->CMDYC > regs->systemclipY2)
-	) {
-		regs->localX = 0;
-		regs->localY = 0;
-	}
+    // VDP1 manual §3.2 : user clip invalide si coin bas-droit < coin haut-gauche
+    if (  ((s16)cmd->CMDXC < (s16)cmd->CMDXA)
+       || ((s16)cmd->CMDYC < (s16)cmd->CMDYA)
+    ) {
+        regs->localX = 0;
+        regs->localY = 0;
+    }
 
   cmd->type = USER_CLIPPING;
   regs->userclipX1 = cmd->CMDXA;
