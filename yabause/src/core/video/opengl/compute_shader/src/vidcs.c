34be2d056962ddbc2cc1a3ca5ece5b0d65da84a6
Benjamin Siskoo <siskoo@tradu-france.com>
2026-04-02 17:16:55 +0200

    vdp2/rgb0: fix sameVDP2RegRBG0 missing CHCTLB/PLSZ/MPOFR comparisons

--- yabause/src/core/video/opengl/compute_shader/src/vidcs.c
+++ yabause/src/core/video/opengl/compute_shader/src/vidcs.c
@@ -4390,14 +4390,14 @@ static int sameVDP2RegRBG0(Vdp2 *a, Vdp2 *b)
   if ((a->CCCTL & 0x0100) != (b->CCCTL & 0x0100)) return 0;
   // Coefficient table
   if ((a->KTCTL & 0x0101) != (b->KTCTL & 0x0101)) return 0;
+  if ((a->CHCTLB & 0x7700) != (b->CHCTLB & 0x7700)) return 0; // colornumber + bitmap RBG0
+  if ((a->PLSZ & 0xFF00)   != (b->PLSZ & 0xFF00))   return 0; // plane size ParaA + ParaB
+  if ((a->MPOFR & 0x77)    != (b->MPOFR & 0x77))     return 0; // map offset RBG0 A+B
 //  if ((a->SFPRMD & 0x300) != (b->SFPRMD & 0x300)) return 0;
-//  if ((a->CHCTLB & 0x7700) != (b->CHCTLB & 0x7700)) return 0;
 //  if ((a->WCTLC & 0xFF) != (b->WCTLC & 0xFF)) return 0;
 //  if ((a->VRSIZE & 0x8000) != (b->VRSIZE & 0x8000)) return 0;
 //  if ((a->RAMCTL & 0x80FF) != (b->RAMCTL & 0x80FF)) return 0;
-//  if ((a->PLSZ & 0xFF00) != (b->PLSZ & 0xFF00)) return 0;
 //  if ((a->KTAOF & 0x707) != (b->KTAOF & 0x707)) return 0;
-//  if ((a->MPOFR & 0x77) != (b->MPOFR & 0x77)) return 0;
 //  if ((a->WCTLD & 0xF) != (b->WCTLD & 0xF)) return 0;
 //  if ((a->BMPNB & 0x7) != (b->BMPNB & 0x7)) return 0;
 //  if ((a->PNCR & 0xFFFF) != (b->PNCR & 0xFFFF)) return 0;
