a2b17ec4381b3b493e5bf3944266fb55712b43e1
Benjamin Siskoo <siskoo@tradu-france.com>
2026-04-08 09:58:07 +0200

    Refactor comments and formatting in UIDebugVDP1.h

--- yabause/src/port/qt/ui/UIDebugVDP1.h
+++ yabause/src/port/qt/ui/UIDebugVDP1.h
@@ -1,20 +1,20 @@
-/*	Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
+/*  Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
 
-	This file is part of Yabause.
+    This file is part of Yabause.
 
-	Yabause is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-	Yabause is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Yabause; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 #ifndef UIDEBUGVDP1_H
 #define UIDEBUGVDP1_H
@@ -25,29 +25,29 @@
 
 class UIDebugVDP1 : public QDialog, public Ui::UIDebugVDP1
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	UIDebugVDP1( QWidget* parent = 0, YabauseLocker* lock = 0);
-	~UIDebugVDP1();
+    UIDebugVDP1( QWidget* parent = 0, YabauseLocker* lock = 0);
+    ~UIDebugVDP1();
 
 protected:
-   u32 *vdp1texture = NULL;
-   u8 *vdp1RawTexture = NULL;
-   int vdp1RawNumBytes;
-   int vdp1texturew, vdp1textureh;
-	 YabauseLocker* mLock;
+    u32 *vdp1texture = NULL;
+    u8  *vdp1RawTexture = NULL;
+    int  vdp1RawNumBytes;
+    int  vdp1texturew, vdp1textureh;
+    YabauseLocker* mLock;
 
 protected slots:
-   void on_lwCommandList_itemSelectionChanged ();
-   void on_lwCommandRaw_itemSelectionChanged ();
-   void on_pbSaveBitmap_clicked ();
-	 void on_pbSaveRawSprite_clicked ();
-	 void syncOnVdp1Entry(int cursel);
-	 void on_pbNextButton_clicked ();
+    void on_lwCommandList_itemSelectionChanged();
+    void on_lwCommandRaw_itemSelectionChanged();
+    void on_pbSaveBitmap_clicked();
+    void on_pbSaveRawSprite_clicked();
+    void syncOnVdp1Entry(int cursel);
+    void on_pbNextButton_clicked();
 
 private:
-	void fillCommandList();
-
+    void fillCommandList();
+    void updateVdp1Registers();
 };
 
 #endif // UIDEBUGVDP1_H
