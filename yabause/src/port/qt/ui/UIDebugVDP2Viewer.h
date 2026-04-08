423525f1c8850d98806655bc2bf0a17de90d091f
Benjamin Siskoo <siskoo@tradu-france.com>
2026-04-08 10:03:07 +0200

    Enhance UIDebugVDP2Viewer with new methods
    Added refresh and updateVdp2Registers methods to UIDebugVDP2Viewer.

--- yabause/src/port/qt/ui/UIDebugVDP2Viewer.h
+++ yabause/src/port/qt/ui/UIDebugVDP2Viewer.h
@@ -1,3 +1,55 @@
+/*  Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
+
+    This file is part of Yabause.
+
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+#ifndef UIDEBUGVDP2VIEWER_H
+#define UIDEBUGVDP2VIEWER_H
+
+#include "ui_UIDebugVDP2Viewer.h"
+#include "../QtYabause.h"
+
+class UIDebugVDP2Viewer : public QDialog, public Ui::UIDebugVDP2Viewer
+{
+    Q_OBJECT
+public:
+    UIDebugVDP2Viewer( QWidget* parent = 0 );
+    void addItem(int i);
+    void clearItems();
+    int  exec() override;
+    void showEvent(QShowEvent *) override;
+    // Call this after a "Next Frame" to refresh both viewer and register tab
+    void refresh();
+
+private:
+    void displayCurrentScreen();
+    void updateVdp2Registers();
+
+protected:
+    pixel_t *vdp2texture;
+    int width, height;
+
+protected slots:
+    void on_cbScreen_currentIndexChanged(int index);
+    void on_pbSaveAsBitmap_clicked();
+    void on_cbOpaque_toggled(bool enable);
+    void on_tabWidget_currentChanged(int index);
+};
+
+#endif // UIDEBUGVDP2VIEWER_H
 /*	Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
 
 	This file is part of Yabause.
