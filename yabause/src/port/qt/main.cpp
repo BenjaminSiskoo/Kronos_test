5b8cfdc4eeb7dfccf6808992e98ecd396d119529
Benjamin Siskoo <siskoo@tradu-france.com>
2026-04-11 00:59:31 +0200

    Implement fullscreen and autostart features in main.cpp
    Added support for fullscreen and autostart features via invokeMethod. Updated main application logic to handle new command-line arguments.

--- yabause/src/port/qt/main.cpp
+++ yabause/src/port/qt/main.cpp
@@ -1,30 +1,15 @@
-/*  Copyright 2005 Guillaume Duhamel
-	Copyright 2005-2006 Theo Berkau
-	Copyright 2008 Filipe Azevedo <pasnox@gmail.com>
-
-	This file is part of Yabause.
-
-	Yabause is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	Yabause is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with Yabause; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+/* Modifié pour Kronos : Support du plein écran et de l'auto-start 
+   via invokeMethod pour contourner les membres protégés.
 */
 #include <QApplication>
+#include <QTimer>
 
 #include "QtYabause.h"
 #include "VolatileSettings.h"
 #include "Settings.h"
 #include "stv.h"
 #include "ui/UIYabause.h"
+
 #ifndef NO_CLI
 #include "Arguments.h"
 #endif
@@ -32,44 +17,66 @@
 int main( int argc, char** argv )
 {
 #ifdef _WIN32
-	//attach the console if the program was started from the console (parent) so that we get console output.
-	auto stdout_type = GetFileType(GetStdHandle(STD_OUTPUT_HANDLE));
-	auto stderr_type = GetFileType(GetStdHandle(STD_ERROR_HANDLE));
-	if (AttachConsole(ATTACH_PARENT_PROCESS)) {
-		if (stdout_type == FILE_TYPE_UNKNOWN)
-		{
-			freopen("CONOUT$", "w", stdout);
-		}
-		if (stderr_type == FILE_TYPE_UNKNOWN)
-		{
-			freopen("CONOUT$", "w", stderr);
-		}
-		//freopen("CONIN$", "r", stdin);
-	}
+    auto stdout_type = GetFileType(GetStdHandle(STD_OUTPUT_HANDLE));
+    auto stderr_type = GetFileType(GetStdHandle(STD_ERROR_HANDLE));
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+        if (stdout_type == FILE_TYPE_UNKNOWN) freopen("CONOUT$", "w", stdout);
+        if (stderr_type == FILE_TYPE_UNKNOWN) freopen("CONOUT$", "w", stderr);
+    }
 #endif
 
-	// create application
-	QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
-	QApplication app( argc, argv );
-	// init application
-	app.setApplicationName( QString( "Kronos v%1" ).arg( VERSION ) );
-	// init settings
-	Settings::setIniInformations();
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    QApplication app( argc, argv );
+
+    app.setApplicationName( QString( "Kronos v%1" ).arg( VERSION ) );
+    Settings::setIniInformations();
+
 #ifdef HAVE_LIBMINI18N
-	// set translation file
-	if ( QtYabause::setTranslationFile() == -1 )
-		qWarning( "Can't set translation file" );
+    if ( QtYabause::setTranslationFile() == -1 )
+        qWarning( "Can't set translation file" );
 #endif
+
 #ifndef NO_CLI
-	Arguments::parse();
+    Arguments::parse();
 #endif
-	// show main window
-	QtYabause::updateTitle();
-	QtYabause::mainWindow()->show();
-	// connection
-	QObject::connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
-	// exec application
-	int i = app.exec();
-	QtYabause::closeTranslation();
-	return i;
+
+    QtYabause::updateTitle();
+    UIYabause* window = QtYabause::mainWindow();
+
+    // --- LOGIQUE PLEIN ÉCRAN ET AUTO-START VIA INVOKE ---
+    QStringList args = QCoreApplication::arguments();
+    bool fullscreen = args.contains("-f") || args.contains("--fullscreen");
+    bool autostart = args.contains("-a") || args.contains("--autostart");
+
+    if (fullscreen) {
+        window->show();
+
+        QTimer::singleShot(500, [window, autostart]() {
+            // On utilise invokeMethod pour appeler le slot protégé 'fullscreenRequested'
+            QMetaObject::invokeMethod(window, "fullscreenRequested", Q_ARG(bool, true));
+
+            if (autostart) {
+                // On cherche l'action de run et on la déclenche
+                QAction* runAction = window->findChild<QAction*>("aEmulationRun");
+                if (runAction) {
+                    runAction->trigger();
+                }
+            }
+        });
+    } else {
+        window->show();
+        if (autostart) {
+            QTimer::singleShot(100, [window]() {
+                QAction* runAction = window->findChild<QAction*>("aEmulationRun");
+                if (runAction) runAction->trigger();
+            });
+        }
+    }
+    // ----------------------------------------------------
+
+    QObject::connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
+    
+    int i = app.exec();
+    QtYabause::closeTranslation();
+    return i;
 }
