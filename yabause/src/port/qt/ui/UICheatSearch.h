efb7a1b31fec6fbdce6fb32475057e4fac6aa64b
Benjamin Siskoo <siskoo@tradu-france.com>
2026-04-15 11:42:04 +0200

    Update UICheatSearch.h

--- yabause/src/port/qt/ui/UICheatSearch.h
+++ yabause/src/port/qt/ui/UICheatSearch.h
@@ -1,26 +1,27 @@
-/*	Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
+/* Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
 
-	This file is part of Yabause.
+    This file is part of Yabause.
 
-	Yabause is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+    Yabause is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-	Yabause is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+    Yabause is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Yabause; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    You should have received a copy of the GNU General Public License
+    along with Yabause; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 #ifndef UICHEATSEARCH_H
 #define UICHEATSEARCH_H
 
 #include "ui_UICheatSearch.h"
 #include "../QtYabause.h"
+#include <QList>
 
 typedef struct
 {
@@ -32,23 +33,26 @@ typedef struct
 
 class UICheatSearch : public QDialog, public Ui::UICheatSearch
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-   UICheatSearch( QWidget* p, QList <cheatsearch_struct> *search, int searchType);
-   QList <cheatsearch_struct> *getSearchVariables(int *searchType);
-protected:
-   QList <cheatsearch_struct> search;
+   UICheatSearch(QWidget* p, QList<cheatsearch_struct> *search, int searchType);
+   virtual ~UICheatSearch();
 
+   QList<cheatsearch_struct> *getSearchVariables(int *searchType);
+
+protected:
+   QList<cheatsearch_struct> search;
    int searchType;
 
    void getSearchTypes();
    void setSearchTypes();
    void listResults();
    void adjustSearchValueQValidator();
+
 protected slots:
    void on_twSearchResults_itemSelectionChanged();
-	void on_leSearchValue_textChanged( const QString & text );
+   void on_leSearchValue_textChanged(const QString &text);
    void on_pbRestart_clicked();
    void on_pbSearch_clicked();
    void on_pbAddCheat_clicked();
@@ -59,4 +63,4 @@ protected slots:
    void on_rb32Bit_toggled(bool checked);
 };
 
-#endif // UICHEATSEARCH_H
+#endif
