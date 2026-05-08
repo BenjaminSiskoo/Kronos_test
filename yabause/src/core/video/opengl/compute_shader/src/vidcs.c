be3071447d02e87c780e5f9edd6b572c7af0bb51
Benjamin Siskoo <siskoo@tradu-france.com>
2026-05-08 13:37:11 +0200

    fix(vdp2): clamp RAMCTL-conflict scan loops to 270 lines

--- yabause/src/core/video/opengl/compute_shader/src/vidcs.c
+++ yabause/src/core/video/opengl/compute_shader/src/vidcs.c
@@ -1216,7 +1216,9 @@ static void Vdp2DrawNBG0(Vdp2* varVdp2Regs, int startLine, int endLine)
   ctrl.info.bitmap_base      = 0;
   ctrl.info.bitmap_wrap_size = 0;
  
-  for (i = 0; i < yabsys.VBlankLineCount; i++) {
+    const int line_max = (yabsys.VBlankLineCount >= 270) ? 270 : yabsys.VBlankLineCount;
+    for (int k = 0; k < line_max; k++) {
+
     ctrl.info.display[i] = isEnabled(NBG0, &Vdp2Lines[i]);
     ctrl.info.enable |= ctrl.info.display[i];
     ctrl.info.alpha_per_line[i] = (u8)(((~Vdp2Lines[i].CCRNA & 0x1F) * 255) / 31);
