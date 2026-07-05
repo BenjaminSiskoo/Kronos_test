13c5b2361f36f23e8a78802539896866e5485abe
Benjamin Siskoo <siskoo@tradu-france.com>
2026-07-05 12:49:22 +0200

    fix(scu-debug): modernize header — explicit, nullptr, override, new private helpers and tab members

--- yabause/src/port/qt/ui/UIDebugSCUDSP.h
+++ yabause/src/port/qt/ui/UIDebugSCUDSP.h
@@ -1,50 +1,53 @@
-/*	Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
+/*  Copyright 2012 Theo Berkau <cwx@cyberwarriorx.com>
 
-	This file is part of Yabause.
-
-	Yabause is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	Yabause is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with Yabause; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    This file is part of Yabause. (GPL v2+)
 */
 #ifndef UIDEBUGSCUDSP_H
 #define UIDEBUGSCUDSP_H
 
 #include "UIDebugCPU.h"
 #include "../QtYabause.h"
+#include <QTabWidget>
+#include <QPlainTextEdit>
 
 class UIDebugSCUDSP : public UIDebugCPU
 {
-	Q_OBJECT
-private:
+    Q_OBJECT
 
 public:
-	UIDebugSCUDSP( YabauseThread *mYabauseThread, QWidget* parent = 0 );
-   void updateRegList();
-   void updateCodeList(u32 addr);
-	void updateAll();
-   u32 getRegister(int index, int *size);
-   void setRegister(int index, u32 value);
-   bool addCodeBreakpoint(u32 addr);
-   bool delCodeBreakpoint(u32 addr);
-   void stepInto();
-   void reserved1();
-   void reserved2();
-   void reserved3();
-   void reserved4();
-   void reserved5();
-protected:
+    explicit UIDebugSCUDSP(YabauseThread *mYabauseThread, QWidget *parent = 0);
+
+    // UIDebugCPU interface — override only where base is virtual
+    void updateRegList();
+    void updateCodeList(u32 addr);
+    void updateAll();           // not virtual in UIDebugCPU base
+    u32  getRegister(int index, int *size);
+    void setRegister(int index, u32 value);
+    bool addCodeBreakpoint(u32 addr);
+    bool delCodeBreakpoint(u32 addr);
+    void stepInto();
+
+    // Save functions (bound to pbReserved1-5)
+    void reserved1();   // Save Program RAM
+    void reserved2();   // Save MD0
+    void reserved3();   // Save MD1
+    void reserved4();   // Save MD2
+    void reserved5();   // Save MD3
+
+private:
+    QString decodeDMAChannel(u32 R, u32 W, u32 C, u32 AD, u32 EN, u32 MD, int ch) const;
+    QString decodeInterruptMask(u32 IMS, u32 IST) const;
+    QString decodeTimers() const;
+    QString formatMDBank(int bank, const u32 *md) const;
+
+    QTabWidget     *m_tabExtra;
+    QPlainTextEdit *m_pteDMA;
+    QPlainTextEdit *m_pteIRQ;
+    QPlainTextEdit *m_pteMD;
+    QPlainTextEdit *m_pteTimers;
 
 protected slots:
+    void onTabChanged(int idx);
 };
 
 #endif // UIDEBUGSCUDSP_H
